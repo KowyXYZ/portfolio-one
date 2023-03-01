82e6f037f8bda7e9284a6c9720cac777400d89a7
KowyXYZ <kowybusiness@gmail.com>
2023-03-01 20:04:20 +0100

    new features

---
+++ src/Components/Home/HeroSection.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+//assets
+import heroImg from '../../Assets/HeroImg.png'
+import downloadBtn from '../../Assets/download.png'
+
+function HeroSection() {
+  return (
+    <div className='flex justify-between items-center mt-24'>
+      <div className='mb-36'>
+        <div>
+            <h1 className=' uppercase text-[80px] font-bold leading-none'>FRONTEND  <span className='text-[#00ADB5]'>Developer</span></h1>
+            <div className='flex justify-betweeen gap-10 mt-12'>
+                <button className=' rounded-3xl border-2 border-[#00ADB5] bg-[#00ADB5] px-6 py-2'>Hire me</button>
+                <button className=' rounded-3xl border-2 border-[#393c41] bg-[#393c41] flex px-6 py-2'>
+                  Download CV  <img src={downloadBtn} alt="" />
+                </button>
+            </div>
+        </div>
+
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+            <img className='w-[650px]' draggable={false} src={heroImg} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div className='flex justify-between items-center pt-6'>
+      <div>
+        <Link to='/'>
+            <h1 className='text-[25px] font-bold'>Kowy</h1>
+        </Link>
+      </div>
+
+      <div className='flex space-x-12 text-[18px] font-light'>
+        <NavLink className='transition-all delay-75 ease-in-out rounded-3xl px-4 py-2' to='/'>Home</NavLink>
+        <NavLink className='transition-all delay-75 ease-in-out rounded-3xl px-4 py-2' to='/about'>About Me</NavLink>
+        <NavLink className='transition-all delay-75 ease-in-out rounded-3xl px-4 py-2' to='/contact'>Contact</NavLink>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Navbar
