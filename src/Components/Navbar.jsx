1f31ecf8bba28e5f1b4bf7daf611e974ace009c5
KowyXYZ <kowybusiness@gmail.com>
2023-03-06 13:50:43 +0100

    started

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,18 +3,19 @@ import { Link, NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div className='flex justify-between items-center pt-6'>
+    <div className='flex justify-center md:justify-between items-center pt-6'>
       <div>
         <Link to='/'>
             <h1 className='text-[25px] font-bold'>Kowy</h1>
         </Link>
       </div>
 
-      <div className='flex space-x-12 text-[18px] font-light'>
+      <div className='hidden md:flex space-x-12 text-[18px] font-light'>
         <NavLink className='transition-all delay-75 ease-in-out rounded-3xl px-4 py-2' to='/'>Home</NavLink>
         <NavLink className='transition-all delay-75 ease-in-out rounded-3xl px-4 py-2' to='/about'>About Me</NavLink>
         <NavLink className='transition-all delay-75 ease-in-out rounded-3xl px-4 py-2' to='/contact'>Contact</NavLink>
       </div>
+      
 
 
     </div>
